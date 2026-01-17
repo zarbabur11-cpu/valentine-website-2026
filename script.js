ac1de2326917da835e406f9517f418721904b2e1
zarbabur11-cpu <zarbabur11@gmail.com>
2026-01-18 03:18:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateConfig() {
     // Check required fields
     if (!config.valentineName) {
         warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "meri jaan";
+        config.valentineName = "";
     }
 
     // Validate colors
